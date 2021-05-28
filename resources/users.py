0f22362a62cae21bb10658accca5d97bc33af62f
John Raymond <jray014@gmail.com>
2021-05-28 08:41:14 -0400

    Users resource created. needs username validation.

---
+++ resources/users.py
@@ -0,0 +1,103 @@
+import schema
+
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user
+from playhouse.shortcuts import model_to_dict
+
+users = Blueprint('users','users')
+
+# REGISTER
+@users.route('/register', methods=['POST'])
+def register():
+    payload = request.get_json()
+
+    payload['email'] = payload['email'].lower()
+    payload['username'] = payload['username'].lower()
+    print(payload)
+
+    try:
+        schema.Users.get(schema.Users.email == payload['email'])
+        #try for username
+        # schema.Users.get(schema.Users.username == payload['username'])
+
+        return jsonify(
+            data={},
+            message=f"A user with the email {payload['email']} already exists",
+            status=401
+        ), 401
+    
+    except schema.DoesNotExist:
+        pw_hash = generate_password_hash(payload['password'])
+
+        created_user = schema.Users.create(
+            username=payload['username'],
+            email=payload['email'],
+            password=pw_hash
+        )
+        print(created_user)
+        login_user(created_user)
+
+        created_user_dict = model_to_dict(created_user)
+        print(created_user_dict)
+
+        created_user_dict.pop('password')
+
+        return jsonify(
+            data=created_user_dict,
+            message=f"Successfully registered user {created_user_dict['email']}",
+            status=201
+        ), 201
+
+
+#LOGIN
+@users.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    payload['email'] = payload['email'].lower()
+    payload['username'] = payload['username'].lower()
+
+    try:
+        user = schema.Users.get(schema.Users.email == payload['email'])
+
+        user_dict = model_to_dict(user)
+
+        password_is_good = check_password_hash(user_dict['password'], payload['password'])
+        if (password_is_good):
+            login_user(user)
+
+            user_dict.pop('password')
+
+            return jsonify(
+                data=user_dict,
+                message=f"Successfully logged in {user_dict['email']}",
+                status=200
+            ), 200
+
+        else:
+            print('pw is no good')
+            return jsonify(
+                data={},
+                message="Email or password is incorrect",
+                status=401
+            ), 401
+
+    except schema.DoesNotExist:
+        print('username is no good')
+        return jsonify(
+            data={},
+            message="Email or password is incorrect", 
+            status=401
+        ), 401
+
+
+# LOGOUT
+@users.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+
+    return jsonify(
+        data={},
+        message='User Successfully logout',
+        status=200,
+    ), 200
