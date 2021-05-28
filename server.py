7543c72d924eccc061d621cffbd8a8b4d24671d5
John Raymond <jray014@gmail.com>
2021-05-28 08:59:43 -0400

    updated server and requirements. Server needs login.

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
+from resources.entries import entries
+from resources.users import users
 
 load_dotenv()
 
@@ -12,6 +14,11 @@ PORT = 8000
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 CORS(app)
+CORS(entries, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
+
+app.register_blueprint(entries, url_prefix='/api/entries')
+app.register_blueprint(users, url_prefix='/api/users')
 
 @app.route('/')
 def index():
