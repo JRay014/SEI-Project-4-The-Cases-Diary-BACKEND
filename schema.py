b2edc0701b2d3d4cc3f54b27b9f134990b912b73
John Raymond <jray014@gmail.com>
2021-05-31 01:48:27 -0400

    models working. backend done for now.

--- schema.py
+++ schema.py
@@ -1,7 +1,12 @@
 from peewee import *
 from flask_login import UserMixin
 
-
+db = PostgresqlDatabase(
+    'Entries',
+    user = 'postgres',
+    password = 'secret',
+    host = 'db.mysite.com'
+)
 
 class Users(UserMixin, Model):
     username = CharField(unique=True)
@@ -19,7 +24,7 @@ class Entries(Model):
     description = CharField()
     decision = CharField()
     keywords = CharField()
-    foreign_key = ForeignKeyField()
+    foreign_key = ForeignKeyField(Users, backref='my_entries')
 
     class Meta:
         database = db
